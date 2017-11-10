3fe9388faee388e9023abc0e3de18c3d1273d9cc
Joshua Abraham <sinisterpatrician@gmail.com>
2017-11-10 13:50:44 -0500

    Update test runner

--- test.sh
+++ test.sh
@@ -26,9 +26,37 @@ else
         echo " - FAIL: FIB DOES NOT RETURN 0 w/ ARGS"
 fi
 
-ret=$(./fibonacci 20)
-if [[ ret == *"20"* ]]; then
-        echo " + Fib converts 20 correctly"
+ret=$(./fibonacci 100)
+if [ $ret == "0x0000000000000000000000000000000000000000000000000000000000000000000000000000001333db76a7c594bfc3" ]; then
+        echo " + Fib works w/ 100"
 else
-        echo " - FAIL: FIB DOES NOT CONVERT 20"
+        echo " - FAIL FIB DOES NOT WORK W/ 100"
+fi
+
+ret=$(./fibonacci 200)
+if [ $ret == "0x0000000000000000000000000000000000000000000000000000000000000338864a5c1caeb07d0ef067cb83df17e395" ]; then
+        echo " + Fib works w/ 200"
+else
+        echo " - FAIL FIB DOES NOT WORK W/ 200"
+fi
+
+ret=$(./fibonacci 300)
+if [ $ret == "0x000000000000000000000000000000000000000000008a4ba39e1a1741497bbbef460a25486ee575f510e921b33e2e10" ]; then
+        echo " + Fib works w/ 300"
+else
+        echo " - FAIL FIB DOES NOT WORK W/ 300"
+fi
+
+ret=$(./fibonacci 400)
+if [ $ret == "0x0000000000000000000000000017322a2cfd320a23266116c4c2c95b3feea3e57fa3d9dfe8b8591e1d72120f26c6fadb" ]; then
+        echo " + Fib works w/ 400"
+else
+        echo " - FAIL FIB DOES NOT WORK W/ 400"
+fi
+
+ret=$(./fibonacci 500)
+if [ $ret == "0x0000000003e3fe615f5f0dad9359c2b1e46ffa400471515e14b7801fd988dea30773c33170414e4e1e2278b212c93d2d" ]; then
+        echo " + Fib works w/ 500"
+else
+        echo " - FAIL FIB DOES NOT WORK W/ 500"
 fi
